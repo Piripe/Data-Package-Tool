21ac09fe0f48b47524c1a41248536c3447097138
Piripe <77517308+Piripe@users.noreply.github.com>
2025-05-04 23:46:27 +0200

    Added pie charts for messages and voice time in the friends tab

--- Data-Package-Tool.UI/Views/Pages/OverviewPages/FriendsViewModel.cs
+++ Data-Package-Tool.UI/Views/Pages/OverviewPages/FriendsViewModel.cs
@@ -9,6 +9,11 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
+using SkiaSharp;
+using System.Security.Cryptography;
 
 namespace DataPackageTool.UI.Views.Pages.OverviewPages
 {
@@ -18,6 +23,10 @@ namespace DataPackageTool.UI.Views.Pages.OverviewPages
         public string? UrlPathSegment => "friends";
         public DataPackage Package { get; set; } = new DataPackage();
         public ObservableCollection<Channel> ChannelsDataGrid { get; set; } = null!;
+
+
+        public ISeries[] MessagesFriendsShareSeries { get; set; } = null!;
+        public ISeries[] VoiceFriendsShareSeries { get; set; } = null!;
         public FriendsViewModel()
         {
             Init();
@@ -30,8 +39,58 @@ namespace DataPackageTool.UI.Views.Pages.OverviewPages
         private void Init()
         {
             HashSet<Channel> channelsDataGrid = Package.Channels.Where(x => x.IsDM()).ToHashSet();
+
+            var messagesFriendsShareSeries = new List<ISeries>();
+            var voiceFriendsShareSeries = new List<ISeries>();
+            int messagesCount = 0;
+            int totalMessageCount = channelsDataGrid.Sum(x => x.Messages.Count);
+            long voiceTime = 0;
+            long totalVoiceTime = channelsDataGrid.Sum(x => x.VoiceTimeIn.Ticks);
+
+            foreach (var channel in channelsDataGrid.OrderByDescending(x=>x.Messages.Count))
+            {
+                long voiceTimeIn = channel.VoiceTimeIn.Ticks;
+                bool validMsgChan = channel.Messages.Count > totalMessageCount * 0.01;
+                bool validVoiceChan = voiceTimeIn > totalVoiceTime * 0.01;
+                //if (!(validMsgChan | validVoiceChan)) c;
+                if (validMsgChan)
+                {
+                    messagesCount += channel.Messages.Count;
+                    messagesFriendsShareSeries.Add(new PieSeries<int>
+                    {
+                        Values = [channel.Messages.Count],
+                        Name = channel.DMRecipient?.DisplayName,
+                        Fill = new SolidColorPaint(SKColor.FromHsl(MD5.HashData(Encoding.UTF8.GetBytes(channel.DMRecipient?.Tag ?? "")).FirstOrDefault() / 255f * 360, 55, 50))
+                    });
+                }
+                if (validVoiceChan) {
+                    voiceTime += voiceTimeIn;
+                    voiceFriendsShareSeries.Add(new PieSeries<long>
+                    {
+                        Values = [voiceTimeIn],
+                        Name = channel.DMRecipient?.DisplayName,
+                        Fill = new SolidColorPaint(SKColor.FromHsl(MD5.HashData(Encoding.UTF8.GetBytes(channel.DMRecipient?.Tag ?? "")).FirstOrDefault() / 255f * 360, 55, 50)),
+                        ToolTipLabelFormatter = (x) => new TimeSpan(x.Model).ToString(@"d\d\ hh\h\ mm\m\ ss\s"),
+                    });
+                }
+            }
+            MessagesFriendsShareSeries = [.. messagesFriendsShareSeries, new PieSeries<int>
+            {
+                Values = [totalMessageCount - messagesCount],
+                Name = "Others",
+                Fill = new SolidColorPaint(new(90, 88, 95))
+            }];
+            VoiceFriendsShareSeries = [.. voiceFriendsShareSeries.OrderByDescending(x=>(x as PieSeries<long>)!.Values!.First()), new PieSeries<long>
+            {
+                Values = [totalVoiceTime - voiceTime],
+                Name = "Others",
+                Fill = new SolidColorPaint(new(90, 88, 95)),
+                ToolTipLabelFormatter = (x) => new TimeSpan(x.Model).ToString(@"d\d\ hh\h\ mm\m\ ss\s"),
+            }];
+
             channelsDataGrid.UnionWith(Package.UsersMap.Values.Where(x => channelsDataGrid.All(y => y.DMRecipientId != x.Id)).Select(x => new Channel() { DMRecipientId = x.Id }).ToHashSet());
             ChannelsDataGrid = new ObservableCollection<Channel>(channelsDataGrid.OrderByDescending(x=>x.Messages.Count));
+
         }
     }
 }
